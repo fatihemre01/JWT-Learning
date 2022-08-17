8daf53507c7e64307572cd4bd727fd05e3dce6cf
Fatih Emre <108624615+fatihemre01@users.noreply.github.com>
2022-08-17 13:50:12 +0300

    Add files via upload

---
+++ authServer.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const jwt = require("jsonwebtoken")
+
+app.use(express.json())
+
+let refreshTokens = []
+
+app.post("/token", (req, res) => {
+    const x = req.body.token
+    if(x == null) return res.sendStatus(401)
+    if(!refreshTokens.includes(x)) return res.sendStatus(403)
+
+    jwt.verify(x, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accesToken = generateAccesToken({name: user.name})
+        res.json({accesToken: accesToken})
+    })
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const user = {name: username}
+
+    const accesToken = generateAccesToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({accesToken: accesToken, refreshToken: refreshToken})
+})
+
+function generateAccesToken(user) {
+    return jwt.sign(user, process.env.ACCES_TOKEN_SECRET, {expiresIn: "15s"})
+}
+
+app.listen(4000, console.log("Running on 4000"))
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const jwt = require("jsonwebtoken")
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: "Kyle",
+        post: "Post1"
+    },
+    {
+        username: "Jim",
+        post: "Post2"
+    }
+]
+
+app.get("/posts", authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username == req.user.name)) 
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, user) => {
+        if(err)  return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000, console.log("Running on 3000"))
